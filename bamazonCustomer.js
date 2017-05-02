738eb38055e731544d3e6a23a13953b8b662a417
Eduardo Acosta <Eduardo Acosta>
2017-05-02 18:41:38 -0400

    2nd commit. Displaying Items and Taking input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,36 +1,56 @@
-// Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-
-
+var inquirer = require("inquirer");
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
-    password: '',
-    database: 'bamazon'
+    password: 'A#47TXon0RPk3!lY4ftP77XNF',
+    database: 'bamazon_db'
 });
 
-// This is where we initiate the MySQL connection
+// This is where we initiate the MySQL connection and test it
 connection.connect(function(err) {
-    if (err) { console.error("error connecting: " + err.stack);
-        return; } 
-    console.log("connected as id " + connection.threadId); });
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    // console.log("connected as id " + connection.threadId); 
+});
 
-    // connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    //   if (error) throw error;
-    //   console.log('The solution is: ', results[0].solution);
-    // });
-
-    connection.end();
-
-
-
-// Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+//First display all of the items available for sale. Include the ids, names, and prices of products for sale.
+var showProducts = function() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw (err);
+        for (var i = 0; i < res.length; i++) {
+            console.log(
+                "Item ID: " + res[i].item_id + "|| Product Name: " + res[i].product_name + "|| Price: " + res[i].price
+            );
+        } // End of for loop   
+        console.log("_________________________");        
+    }); // End of connection query 
+}; // End of showProducts
+showProducts();
 
 // The app should then prompt users with two messages.
-
 // The first should ask them the ID of the product they would like to buy.
+var runSearch = function() {
+    inquirer.prompt({
+        name: "itemChoice",
+        type: "input",
+        message: "\nWelcome to BAMazon, the greatest frigging store in the world. Type in the Item ID for the product you want to buy:"
+    }).then(function(answer) {
+            connection.query("Select * from products where item_id ='" + answer.itemChoice + "'"),
+                function(err, res) {
+                    if (err) throw err;
+                    console.log("Good");
+                };        
+    });
+    //End of then()
+} //End of runSearch()
+runSearch();
+
 // The second message should ask how many units of the product they would like to buy.
+
 // Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
 
 // If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
@@ -39,6 +59,3 @@ connection.connect(function(err) {
 // This means updating the SQL database to reflect the remaining quantity.
 // Once the update goes through, show the customer the total cost of their purchase.
 // If this activity took you between 8-10 hours, then you've put enough time into this assignment. Feel free to stop here -- unless you want to take on the next challenge.
-
-
-           
